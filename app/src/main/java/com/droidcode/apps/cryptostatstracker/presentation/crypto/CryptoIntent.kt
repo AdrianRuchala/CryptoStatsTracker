d1c08a0c4d6d96d7605f586d6cece746468d9cef
AdrianRuchala <adrianruchala03@gmail.com>
2025-02-19 16:43:43 +0100

    T3: Implement coin search functionality. Added `IsCoinExisting` intent, search bar error handling, and `coin_search_error` string.

--- app/src/main/java/com/droidcode/apps/cryptostatstracker/presentation/crypto/CryptoIntent.kt
+++ app/src/main/java/com/droidcode/apps/cryptostatstracker/presentation/crypto/CryptoIntent.kt
@@ -3,4 +3,9 @@ package com.droidcode.apps.cryptostatstracker.presentation.crypto
 sealed class CryptoIntent {
     object LoadTop10Crypto : CryptoIntent()
     data class LoadCoinData(val coinId: String) : CryptoIntent()
+    data class IsCoinExisting(
+        val coinId: String,
+        val onSuccess: (String) -> Unit,
+        val onError: (Boolean) -> Unit
+    ) : CryptoIntent()
 }
